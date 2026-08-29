c16d221cb1eac3f9f935f9ab67c55567728bf58d
Seemeebee <brandydalexander@gmail.com>
2026-08-29 15:58:37 -0500

    question.jsx change

--- Path to Peace/src/components/Question.jsx
+++ Path to Peace/src/components/Question.jsx
@@ -4,15 +4,14 @@ function Question({ question, selectedAnswer, onAnswer }) {
   return (
     <section className="question-card">
       {/* Displays the question */}
-
       <h3>{question.question}</h3>
-      {/* Displays all answer choices */}
 
+      {/* Displays all answer choices */}
       {question.options.map((option) => (
         <button
           className="answer-option"
           key={option.text}
-          onClick={() => onAnswer(option)}
+          onClick={() => onAnswer(question.id, option.score)}
         >
           {option.text}
         </button>
